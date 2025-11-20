500254f2138188ccd2d743f13bb43ed8db1f222f
Chinadoc <jeremy.samuels17@gmail.com>
2025-11-20 07:45:18 -0600

    fix(typer): Complete SearchHeader integration with all required props

--- app/typer/page.tsx
+++ app/typer/page.tsx
@@ -7,9 +7,16 @@ export default function TyperPage() {
     return (
         <div className="flex flex-col h-screen">
             <SearchHeader
+                query=""
+                setQuery={() => { }}
+                handleSearch={() => { }}
+                handleKeyPress={() => { }}
+                isLoading={false}
                 activeMainTab="typer"
                 activeTranslation="afghan2023"
-                onTranslationChange={() => { }}
+                setActiveTranslation={() => { }}
+                searchLanguage="pashto"
+                isEnglishMode={false}
             />
             <PashtoTyper />
         </div>
