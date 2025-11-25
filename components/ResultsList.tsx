07e676f26dfc730a3c9d0ba770241cbfc4e29224
Chinadoc <jeremy.samuels17@gmail.com>
2025-11-24 19:01:07 -0600

    fix: Move import to top level in ResultsList.tsx to fix build error

--- components/ResultsList.tsx
+++ components/ResultsList.tsx
@@ -15,6 +15,13 @@ import HighlightText from './HighlightText';
 import EnhancedHighlightText from './EnhancedHighlightText';
 import VirtualizedResults from './VirtualizedResults';
 import VerbDetails from './VerbDetails';
+import {
+  matchesPerson,
+  matchesTense,
+  matchesMood,
+  matchesAspect,
+  normalizeLabel
+} from '@/app/utils/verb-filters';
 
 const OT_BOOKS = new Set([
   'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi'
@@ -792,13 +799,7 @@ export default function ResultsList({ results, audioMap, loading, query, terms: 
     }
   };
 
-  import {
-    matchesPerson,
-    matchesTense,
-    matchesMood,
-    matchesAspect,
-    normalizeLabel
-  } from '@/app/utils/verb-filters';
+
 
   // ... existing imports ...
 
